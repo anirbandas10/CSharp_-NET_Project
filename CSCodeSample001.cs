cf914e718a8e1f0d08a1cdb307cf51e91145d709
anirbandas10 <anirbandas1127@gmail.com>
2021-09-30 00:02:50 +0530

    Update

---
+++ CSCodeSample001.cs
@@ -0,0 +1,21 @@
+/*c# basic program to declare different type of variables and
+assigning them with the values and then print the values*/
+
+using System;
+
+class HelloWorld {
+	static void Main() {
+		//declaring different type of variables and
+		//assigning them with the values
+		char char_value = 'X';
+		int int_value = 100;
+		float float_value = 10.23f;
+		string string_value = "IncludeHelp";
+
+		//printing the values
+		Console.WriteLine("char_value = " + char_value);
+		Console.WriteLine("int_value = " + int_value);
+		Console.WriteLine("float_value = " + float_value);
+		Console.WriteLine("string_value = " + string_value);
+	}
+}
